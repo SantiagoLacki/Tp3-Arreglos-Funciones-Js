0bdf6f615c215d19b052e35ecff72118e2d0cd4d
Santiago Lacki <santiagolacki@gmail.com>
2025-04-28 19:06:53 -0300

    Se agrego el enunciado del ejercicio2 y parte de la resolucion

---
+++ Js/Ejercicio2.js
@@ -0,0 +1,68 @@
+
+// 2-  Crear un script que solicite al usuario 
+// mediante un prompt el nombre de ciudades y 
+// almacenarlas en un arreglo, cuando el usuario 
+// selecciona cancelar se debe mostrar el arreglo 
+// generado, luego realizar las siguientes 
+// acciones:
+
+// Mostrar la longitud del arreglo.
+// Mostrar en el documento web los ítems de las 
+// posiciones primera, tercera y última.
+// Añade en última posición la ciudad de París.
+// Escribe por pantalla el elemento que ocupa la 
+// segunda posición.
+// Sustituye el elemento que ocupa la segunda 
+// posición por la ciudad de 'Barcelona'.
+// Ejemplo:
+// Input:
+// [‘Nueva York, Estados Unidos’, ‘Barcelona, 
+// España’, ‘Tokio, Japón’, ‘Londres, Reino Unido’,
+// ‘Roma, Italia’, ‘Pekín, China’, ‘Río de Janeiro, 
+// Brasil’, ‘Ámsterdam, Países Bajos’, ‘Sídney, 
+// Australia’, ‘El Cairo, Egipto’]
+
+const ciudades = [];
+let ciudad = "";
+
+do {
+  ciudad = prompt("Ingresa el nombre de una ciudad: ");
+  ciudades.unshift(ciudad);
+}
+while (confirm("¿Desea ingresar otra ciudad? "))
+
+console.log(ciudades.length);
+document.writeln(`La longitud del arreglo CIUDADES es de: ${ciudades.length}`);
+
+document.writeln(`<h3>Array generado.</h3>`);
+
+document.writeln(`<ul>`)
+for(let i = 0; i < ciudades.length; i++) {
+  document.writeln(`<li>${ciudades[i]}</li>`)
+}
+document.writeln(`</ul>`)
+
+document.writeln(`<h3>Elementos del array de la primera, tercera y ultima posicion.</h3>`);
+
+document.writeln(`<ul>`)
+for(let i = 0; i < ciudades.length; i++) {
+  document.writeln(`<li>${ciudades[0]}</li>`)
+  document.writeln(`<li>${ciudades[2]}</li>`)
+  document.writeln(`<li>${ciudades[ciudades.length-1]}</li>`)
+}
+document.writeln(`</ul>`)
+
+document.writeln(`<h3>Se agrega la ciudad Paris al final del array.</h3>`);
+
+ciudades.unshift("Paris");
+
+document.writeln(`<ul>`)
+for(let i = 0; i < ciudades.length; i++) {
+  document.writeln(`<li>${ciudades[i]}</li>`)
+}
+document.writeln(`</ul>`)
+
+document.writeln(`<h3>Elemento de la segunda posicion del array.</h3>`);
+
+document.writeln(`${ciudades[1]}`)
+
